57263043bd59b3df2f8f28cce1d41f273628550c
Max Valentine <107592271+1027774sae@users.noreply.github.com>
2022-06-16 10:52:42 +1000

    Initial commit

---
+++ Assets/Scripts/WeekTwoActivities.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace MaxValentine
+{
+    public class WeekTwoActivities : MonoBehaviour
+    {
+        // Variables go here
+        private string favouriteGame = "Hollow Knight";
+        private int hoursPlayed = 155;
+        private float gamePrice = 17.5f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("This is string 1. " + "This is string 2");
+            Debug.Log("My favourite game is " + favouriteGame + ", I have played it for " + hoursPlayed + " hours and it cost me $" + gamePrice + ". Therefore, my value of dollars per hour is $" + gamePrice / hoursPlayed + ".");
+
+            //int randomNumber = Random.RandomRange(1f,10f)
+        }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 22T2_GAD170_MAX_VALENTINE_WeeklyActivities
+ My weekly activities for SAE with Aaron Goss
