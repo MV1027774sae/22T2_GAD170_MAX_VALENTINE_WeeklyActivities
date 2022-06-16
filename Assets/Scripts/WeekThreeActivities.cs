c64f1026cc66e8365d88e486e97b772ca774ad3c
Max Valentine <107592271+1027774sae@users.noreply.github.com>
2022-06-16 11:40:25 +1000

    Tried to complete the 1st, 2nd activity week 3

---
+++ Assets/Scripts/WeekThreeActivities.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeekThreeActivities : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+  
+        { public int strength = Random.Range(0, 21);
+    public int agility = Random.Range(0, 21);
+    public int intelligence = Random.Range(0, 21); }
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+}
+
+void MyName()
+{
+    Debug.Log("Max Valentine");
+
+}
